7b085690ac0398f801902dc2bbab8f6265902d2e
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-10-19 20:18:58 +0500

    Update class-our-brands-widget.php

--- elementor-widgets/class-our-brands-widget.php
+++ elementor-widgets/class-our-brands-widget.php
@@ -33,11 +33,69 @@ class OurBrands_Widget extends \Elementor\Widget_Base {
 						'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
 				]
 		);
-		$this->add_group_control(
-				Group_Control_Typography::get_type(),
+		// Create the repeater for brand logos
+		$repeater = new \Elementor\Repeater();
+		// Add control for logo image
+		$repeater->add_control(
+				'brand_logo',
 				[
-						'name'     => 'typography',
-						'selector' => '{{WRAPPER}}  .elementor-heading-title',
+						'label'   => __( 'Choose Brand Logo', 'fractal' ),
+						'type'    => \Elementor\Controls_Manager::MEDIA,
+						'default' => [
+								'url' => \Elementor\Utils::get_placeholder_image_src(),
+						],
+				]
+		);
+		// Add control for brand name (alt text)
+		$repeater->add_control(
+				'brand_name',
+				[
+						'label'       => __( 'Brand Name (Alt Text)', 'fractal' ),
+						'type'        => \Elementor\Controls_Manager::TEXT,
+						'default'     => __( 'Brand Name', 'fractal' ),
+						'label_block' => true,
+				]
+		);
+		// Repeater control for brands
+		$this->add_control(
+				'brands_list',
+				[
+						'label'       => __( 'Brands', 'fractal' ),
+						'type'        => \Elementor\Controls_Manager::REPEATER,
+						'fields'      => $repeater->get_controls(),
+						'default'     => [
+								[
+										'brand_name' => __( 'Apple', 'fractal' ),
+										'brand_logo' => [
+												'url' => get_template_directory_uri() . '/fractal/build/apple.webp',
+										],
+								],
+								[
+										'brand_name' => __( 'Disney', 'fractal' ),
+										'brand_logo' => [
+												'url' => get_template_directory_uri() . '/fractal/build/disney.webp',
+										],
+								],
+								[
+										'brand_name' => __( 'Google', 'fractal' ),
+										'brand_logo' => [
+												'url' => get_template_directory_uri() . '/fractal/build/google.webp',
+										],
+								],
+								[
+										'brand_name' => __( 'Meta', 'fractal' ),
+										'brand_logo' => [
+												'url' => get_template_directory_uri() . '/fractal/build/meta.webp',
+										],
+								],
+								[
+										'brand_name' => __( 'Nike', 'fractal' ),
+										'brand_logo' => [
+												'url' => get_template_directory_uri() . '/fractal/build/nike.webp',
+										],
+								],
+						],
+						'title_field' => '{{{ brand_name }}}',
 				]
 		);
 		$this->end_controls_section();
@@ -45,26 +103,38 @@ class OurBrands_Widget extends \Elementor\Widget_Base {
 
 	protected function render() {
 		$settings = $this->get_settings_for_display();
-		include get_stylesheet_directory() . '/elementor-templates/custom-ourbrands-template.php';
+		if ( ! empty( $settings['brands_list'] ) ) {
+			echo '<div class="w-full">';
+			echo '<section class="bg-general-white self-stretch flex flex-col items-center justify-center py-[98px]">';
+			echo '<div class="max-w-[1170px] w-full flex flex-wrap overflow-x-auto items-center justify-between gap-4 scroll-snap-x scroll-snap-start scroll-smooth ">';
+			// Loop through each brand in the repeater
+			foreach ( $settings['brands_list'] as $item ) {
+				$brand_logo = isset( $item['brand_logo']['url'] ) ? esc_url( $item['brand_logo']['url'] ) : '';
+				$brand_name = isset( $item['brand_name'] ) ? esc_attr( $item['brand_name'] ) : '';
+				if ( $brand_logo ) {
+					echo '<img class="relative h-16" alt="' . $brand_name . '" src="' . $brand_logo . '">';
+				}
+			}
+			echo '</div>';
+			echo '</section>';
+			echo '</div>';
+		}
 	}
 
 	protected function _content_template() {
 		?>
+		<#
+		if ( settings.brands_list.length ) { #>
 		<div class="w-full">
-	<section class="bg-general-white self-stretch flex flex-col items-center justify-center py-[98px]">
-
-		<div class="max-w-[1170px] w-full flex flex-wrap overflow-x-auto items-center
-	justify-between flex-wrap mq925:flex-nowrap sm:justify-between gap-4 mq925:gap-[50px] scroll-snap-x scroll-snap-start scroll-smooth ">
-			<div class="shrink-0 w-[30vw] hidden mq925:block"></div>
-			<img class="relative h-16 " alt="" src="/wp-content/themes/fractal/fractal/build/apple.webp">
-
-			<img class="relative h-16 " alt="" src="/wp-content/themes/fractal/fractal/build/disney.webp">
-			<img class="relative h-16 " alt="" src="/wp-content/themes/fractal/fractal/build/google.webp">
-			<img class="relative h-16 " alt="" src="/wp-content/themes/fractal/fractal/build/meta.webp">
-			<img class="relative h-16 " alt="" src="/wp-content/themes/fractal/fractal/build/nike.webp">
+			<section class="bg-general-white self-stretch flex flex-col items-center justify-center py-[98px]">
+				<div class="max-w-[1170px] w-full flex flex-wrap overflow-x-auto items-center justify-between gap-4 scroll-snap-x scroll-snap-start scroll-smooth ">
+					<# _.each( settings.brands_list, function( item ) { #>
+					<img class="relative h-16" alt="{{ item.brand_name }}" src="{{ item.brand_logo.url }}">
+					<# }); #>
+				</div>
+			</section>
 		</div>
-	</section>
-</div>
+		<# } #>
 		<?php
 	}
 }
